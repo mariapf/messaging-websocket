7e0f482d9f0bcb6e26ccc810f5c37fc1bbadb122
mariapf <mariadelmarpf@gmail.com>
2020-04-30 10:36:24 +0200

    initial commit

---
+++ src/test/java/com/mariapf/messagingwebsocket/MessagingWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariapf.messagingwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
